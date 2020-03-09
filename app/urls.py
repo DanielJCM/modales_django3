bd7c0dbcf474164021ceed2b488910d7b06c7b81
Daniel Contreras <exodiadaniel@gmail.com>
2020-03-09 08:50:27 -0400

    Primer commit

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.urls import path
+from app import views
+
+
+urlpatterns = [
+    path('', views.Index.as_view(), name='index'),
+    path('list', views.List.as_view(), name='list'),
+    path('create/', views.Create.as_view(), name='create'),
+    path('update/<int:pk>/', views.Update.as_view(), name='update'),
+    path('details/<int:pk>/', views.Detail.as_view(), name='details'),
+    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
+]
---
+++ app/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from app.models import Person
+from app.forms import PersonForm, PersonUpdateForm
+from django.shortcuts import render
+
+
+class Index(TemplateView):
+    """
+    Class that shows the start template.
+    """
+    template_name = "app/index.html"
+
+
+class List(ListView):
+    """
+    Class that lists the Person objects.
+    """
+    model = Person
+
+
+class Create(CreateView):
+    """
+    Class that allows you to create and save a Person object.
+    """
+    model = Person
+    form_class = PersonForm
+    #fields = ['first_name', 'last_name']
+    success_url = reverse_lazy('list')
+
+
+class Update(UpdateView):
+    """
+    Class that allows you to update the data of a Person object.
+    """
+    model = Person
+    form_class = PersonUpdateForm
+    template_name = "app/person_update.html"
+    #fields = ['first_name', 'last_name']
+    success_url = reverse_lazy('list')
+
+
+class Delete(DeleteView):
+    """
+    Class that allows you to delete a Person object.
+    """
+    model = Person
+    success_url = reverse_lazy('list')
+
+class Detail(DetailView):
+    """
+    Class that allows you to see the details of a Person object
+    """
+    model = Person
+    template_name = 'app/person_detail.html'
+
+# Create your views here.
